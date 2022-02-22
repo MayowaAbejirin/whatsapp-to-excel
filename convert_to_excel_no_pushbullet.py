aaf8bd05295645d218d7983a4e3d7705d695d734
MayowaAbejirin <mayowaabejirin@gmail.com>
2022-02-22 08:17:00 +0100

    first commit

---
+++ convert_to_excel_no_pushbullet.py
@@ -0,0 +1,41 @@
+import urllib.request
+import os
+import pandas as pd # pip install pandas openpyxl
+
+
+
+# get Chatfile
+file_path = "chat.txt"  #chat filename here
+
+# read file by lines
+with open(file_path, mode='r', encoding="utf8") as f:
+    data = f.readlines()
+
+# FOUND ON GITHUB: https://gist.github.com/kwcooper/a21ba58272d3cdf26310cc02ee4b168f
+# parse text, create list of lists structure & remove first whatsapp info message
+dataset = data[1:]
+cleaned_data = []
+for line in dataset:
+    # Check, whether it is a new line or not
+    # If the following characters are in the line -> assumption it is NOT a new line
+    if '/' in line and ':' in line and ',' in line and '-' in line:
+        # grab the info and cut it out
+        date = line.split(",")[0]
+        line2 = line[len(date):]
+        time = line2.split("-")[0][2:]
+        line3 = line2[len(time):]
+        name = line3.split(":")[0][4:]
+        line4 = line3[len(name):]
+        message = line4[6:-1] # strip newline charactor
+        cleaned_data.append([date, time, name, message])
+
+    # else, assumption -> new line. Append new line to previous 'message'
+    else:
+        new = cleaned_data[-1][-1] + " " + line
+        cleaned_data[-1][-1] = new
+
+# Create the DataFrame
+df = pd.DataFrame(cleaned_data, columns = ['Date', 'Time', 'Name', 'Message'])
+
+# Save it!
+df.to_excel('chat_history.xlsx', index=False)  #excel filename here
